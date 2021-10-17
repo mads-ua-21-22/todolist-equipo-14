e0d5f30bc2ac56db5438eceb33c68b8a320a1adb
Carlos Poveda Cañizares <cpc73@alu.ua.es>
2021-10-17 16:07:46 +0200

    Funcionando barra de menu en página 'Acerca de'

--- src/main/java/madstodolist/controller/HomeController.java
+++ src/main/java/madstodolist/controller/HomeController.java
@@ -1,15 +1,42 @@
 package madstodolist.controller;
 
+import madstodolist.authentication.ManagerUserSession;
+import madstodolist.model.Usuario;
+import madstodolist.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
 
+    @Autowired
+    UsuarioService usuarioService;
+    @Autowired
+    ManagerUserSession managerUserSession;
+
     @GetMapping("/about")
-    public String about(Model model) {
-        return "about";
+    public String about(Model model, HttpSession session) {
+        Long idUsuario = managerUserSession.usuarioLogeado(session);
+        Usuario usuario = null;
+
+        if(idUsuario != null) {
+            managerUserSession.comprobarUsuarioLogeado(session, idUsuario);
+            usuario = usuarioService.findById(idUsuario);
+
+            model.addAttribute("usuario", usuario);
+            model.addAttribute("aux", 0);
+            return  "about";
+
+        }
+        else {
+            model.addAttribute("usuario", usuario);
+            model.addAttribute("aux", 1);
+            return "about";
+        }
     }
 
 }
