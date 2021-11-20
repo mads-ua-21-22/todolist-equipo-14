5d0db919c05c2fae9d7fcc0c023d46703d33b9c0
Carlos Poveda Cañizares <cpc73@alu.ua.es>
2021-11-20 11:32:17 +0100

    Añadido el compo descripcion a EquipoData

--- src/main/java/madstodolist/controller/EquipoData.java
+++ src/main/java/madstodolist/controller/EquipoData.java
@@ -2,6 +2,7 @@ package madstodolist.controller;
 
 public class EquipoData {
     private String nombre;
+    private String descripcion;
 
     public String getNombre() {
         return nombre;
@@ -9,5 +10,13 @@ public class EquipoData {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
 }
