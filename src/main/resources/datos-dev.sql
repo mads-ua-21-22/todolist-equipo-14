c8f092091f7ff73414f7dc669fa55b183c986124
Carlos Poveda Cañizares <cpc73@alu.ua.es>
2021-12-05 01:35:37 +0100

    Columnas centradas

--- src/main/resources/datos-dev.sql
+++ src/main/resources/datos-dev.sql
@@ -2,8 +2,6 @@
 INSERT INTO usuarios (id, email, nombre, password, fecha_nacimiento) VALUES('1', 'user@ua', 'Usuario Ejemplo', '123', '2001-02-10');
 INSERT INTO tareas (id, titulo, usuario_id, estado) VALUES('1', 'Lavar coche', '1', 'todo');
 INSERT INTO tareas (id, titulo, usuario_id, estado) VALUES('2', 'Renovar DNI', '1', 'inprogress');
-INSERT INTO tareas (id, titulo, usuario_id, estado) VALUES('3', 'ifoweifnwoeifnwo', '1', 'inprogress');
-INSERT INTO tareas (id, titulo, usuario_id, estado) VALUES('4', 'YYYYYYYYYY', '1', 'done');
 INSERT INTO usuarios (id, email, nombre, password, fecha_nacimiento) VALUES('2', 'juan.gutierrez@gmail.com', 'Juan Gutiérrez', '123', '1997-02-20');
 INSERT INTO usuarios (id, email, nombre, password, fecha_nacimiento) VALUES('3', 'carlos@gmail.com', 'Carlos', '123', '1994-09-20');
 INSERT INTO equipos (id,nombre) VALUES ('1','EQUIPO1');
