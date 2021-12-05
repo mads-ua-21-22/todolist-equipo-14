4c2ee305a1505bf121a15dee1915d1544211692d
Carlos Poveda Cañizares <cpc73@alu.ua.es>
2021-12-05 17:28:00 +0100

    modificados nombres estados

--- src/main/resources/datos-dev.sql
+++ src/main/resources/datos-dev.sql
@@ -1,7 +1,7 @@
 /* Populate tables */
 INSERT INTO usuarios (id, email, nombre, password, fecha_nacimiento) VALUES('1', 'user@ua', 'Usuario Ejemplo', '123', '2001-02-10');
-INSERT INTO tareas (id, titulo, usuario_id, estado) VALUES('1', 'Lavar coche', '1', 'todo');
-INSERT INTO tareas (id, titulo, usuario_id, estado) VALUES('2', 'Renovar DNI', '1', 'inprogress');
+INSERT INTO tareas (id, titulo, usuario_id, estado) VALUES('1', 'Lavar coche', '1', 'To Do');
+INSERT INTO tareas (id, titulo, usuario_id, estado) VALUES('2', 'Renovar DNI', '1', 'In Progress');
 INSERT INTO usuarios (id, email, nombre, password, fecha_nacimiento) VALUES('2', 'juan.gutierrez@gmail.com', 'Juan Gutiérrez', '123', '1997-02-20');
 INSERT INTO usuarios (id, email, nombre, password, fecha_nacimiento) VALUES('3', 'carlos@gmail.com', 'Carlos', '123', '1994-09-20');
 INSERT INTO equipos (id,nombre) VALUES ('1','EQUIPO1');
